78559e3ec57338783f1417d2e01001f883db2547
heawon <okhw97@gmail.com>
2021-10-03 12:54:20 +0900

    initial setting

---
+++ search-pic/src/main/java/dimage/searchpic/SearchPicApplication.java
@@ -0,0 +1,13 @@
+package dimage.searchpic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchPicApplication.class, args);
+	}
+
+}
---
+++ search-pic/src/test/java/dimage/searchpic/SearchPicApplicationTests.java
@@ -0,0 +1,13 @@
+package dimage.searchpic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchPicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
