4d83d6e19b367ee3476927246a504a60dd26b878
heawon <okhw97@gmail.com>
2021-11-11 16:21:55 +0900

    Feat: request logging 추가

---
+++ search-pic/src/main/java/dimage/searchpic/config/LogAspect.java
@@ -0,0 +1,27 @@
+package dimage.searchpic.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+    @Around("execution(* dimage.searchpic.controller..*(..))")
+    public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        long start = System.currentTimeMillis();
+        try {
+            return joinPoint.proceed(joinPoint.getArgs());
+        } finally {
+            long end = System.currentTimeMillis();
+            log.info("method = {}, url = {}, time = {} ms", request.getMethod(), request.getRequestURI(), end-start);
+        }
+    }
+}
