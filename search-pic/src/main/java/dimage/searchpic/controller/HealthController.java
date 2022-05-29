d277788a098125ccb8c3f101e7f8e62ab9989587
osandra <okhw97@gmail.com>
2022-05-29 13:08:25 +0900

    Add HealthController

---
+++ search-pic/src/main/java/dimage/searchpic/controller/HealthController.java
@@ -0,0 +1,13 @@
+package dimage.searchpic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping
+    public String healthCheck() {
+        return "OK";
+    }
+}
